070e0cc97018e8859cb619c9d644b8642406a755
jamoroso-caylent <jose.amoroso@caylent.com>
2022-07-03 21:09:37 -0500

    Update hostname

--- angular-14-client/src/app/services/tutorial.service.ts
+++ angular-14-client/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'https://api.jamoroso/api/tutorials';
+const baseUrl = 'https://api.jamoroso.com/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
