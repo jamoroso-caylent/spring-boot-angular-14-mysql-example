ead071c31d6ad664a553cd6bd3cf639229f4c067
jamoroso-caylent <jose.amoroso@caylent.com>
2022-07-09 04:09:57 -0500

    Update cors on java app

--- spring-boot-server/src/main/java/com/bezkoder/spring/datajpa/controller/TutorialController.java
+++ spring-boot-server/src/main/java/com/bezkoder/spring/datajpa/controller/TutorialController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bezkoder.spring.datajpa.model.Tutorial;
 import com.bezkoder.spring.datajpa.repository.TutorialRepository;
 
-@CrossOrigin(origins = "https://qa.jamoroso.com")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
